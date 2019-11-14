397b93222b4c73d2258ad74498a6a50c6b847f20
Ethan <lza104@sfu.ca>
2019-11-14 04:17:21 -0800

    New changes.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+const cursor = document.querySelector(".cursor");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
@@ -8,3 +9,15 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add("expand");
+ 
+  setTimeout(() => {
+      cursor.classList.remove("expand");
+  }, 500)
+})
